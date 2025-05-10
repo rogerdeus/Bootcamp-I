6871ddfebb9c7a248081b8ba37d97fc5ba449620
ZesseZ <138524586+ZesseZ@users.noreply.github.com>
2025-05-09 22:45:18 -0300

    Update pss(ColabVitor).c

--- Projetos de interesse pessoal/pss(ColabVitor).c
+++ Projetos de interesse pessoal/pss(ColabVitor).c
@@ -3,6 +3,24 @@
 
 int main(){
   setlocale(LC_ALL, "Portuguese");
+  //Iremos fazer um verificador de triângulo, que caso confirmado, calcula seu perímetro
+
+  float lado1, lado2, lado3, perimetro;
+
+    // Entrada de dados
+    printf("Digite o primeiro lado: ");
+    scanf("%f", &lado1);
+
+    printf("Digite o segundo lado: ");
+    scanf("%f", &lado2);
+
+    printf("Digite o terceiro lado: ");
+    scanf("%f", &lado3);
+
+    // Verifica se os lados formam um triângulo
+    if ((lado1 + lado2 > lado3) &&
+        (lado1 + lado3 > lado2) &&
+        (lado2 + lado3 > lado1)) {}
   
   return 0;
 }
