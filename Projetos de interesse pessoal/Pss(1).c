92e7d9ae30440b7503fdde6e02a551ea37c19bc2
ZesseZ <138524586+ZesseZ@users.noreply.github.com>
2025-05-10 17:56:43 -0300

    Update Pss(1).c

--- Projetos de interesse pessoal/Pss(1).c
+++ Projetos de interesse pessoal/Pss(1).c
@@ -6,17 +6,6 @@
 #include<locale.h>
 
 int main(){
-    setlocale(LC_ALL, "Portuguese");
-
-    float b, c;
-
-    printf("\n\nInforme o tamanho dos dois catetos de um triângulo equilátero para saber o tamanho de sua hipotenusa: ");
-    scanf("%f %f", &b, &c);
-
-    printf("\n\nO cateto b, vale: %.2f", b);
-    printf("\n\nO cateto c, vale: %.2f", c);
-
-    printf("\n\nSabendo que a fórmula do teorema de pitágoras é: a² = b² + c², é possível concluir que (a) é igual a: %.2f", sqrt(pow(b, 2) + pow(c, 2)));
-
+  
     return 0;
 }
